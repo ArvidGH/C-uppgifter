1f3b428d773cfdb05a07c527902d75709302496c
ArvidGH <91956340+ArvidGH@users.noreply.github.com>
2022-08-31 09:35:25 +0200

    Add files via upload

---
+++ uppgifter Arvid GH/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace uppgifter_Arvid_GH
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1. Console.WriteLine("Hello World!");
+            /*2. Console.WriteLine("Once upon a time"); Console.WriteLine("I was falling in love"); Console.WriteLine("Now I’m only falling apart");*/
+            /*3.Console.WriteLine("Once upon a time\nI was falling in love\nNow I’m only falling apart");*/
+            /*4.string text = "Live and sleep"; Console.WriteLine(text);*/
+            /*5.string name = "Hej Ada LoveLace!"; Console.WriteLine(name);*/
+            /*6.string hej = Console.ReadLine(); Console.WriteLine(hej);*/
+            /*7.string hej = Console.ReadLine();
+            int i = 0;
+            while (i < 3)
+            {
+                Console.WriteLine(hej);
+                i++;
+            }
+            */
+            /*8.
+            Console.WriteLine("Vad heter du?");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Hello " + namn + "!");*/
+            /*9.
+            Console.WriteLine("Tjenare mannen. Hur är det?");
+            Console.ReadLine();
+            Console.WriteLine("Gött. Hur gammal är du?");
+            Console.ReadLine();
+            Console.WriteLine("Jahopp");*/
+            /*10.
+            Console.WriteLine("Ska berätta en liten historia. Vad heter du?");
+            string namn = Console.ReadLine();
+            Console.WriteLine("Vilket är ditt jobb?");
+            string jobb = Console.ReadLine();
+            Console.WriteLine($"Det var en gång en persone som hette {namn} och som arbetade som {jobb}. {namn} spenderade all sin tid på sitt jobb.\nHelt plötsligt märkte {namn} att hela livet hade gått åt till att jobba.");
+            */
+            /*11.
+            int days = 200;
+            double hours = 3.5;
+            string coding = "It sure is!";
+            Console.WriteLine("Days to summer:");
+            Console.WriteLine(days);
+            Console.WriteLine("Hours to lunch:");
+            Console.WriteLine(hours);
+            Console.WriteLine("Coding is fun:");
+            Console.WriteLine(coding);
+            */
+            /*12.
+            Console.WriteLine("Skriv ett nummer");
+            int val = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val);
+            Console.WriteLine("Skriv ett nummer");
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(val1);
+            */
+            /*13.*/
+            double val = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(val);
+            double val1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(val1);
+        }
+    }
+}
